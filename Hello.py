0ac798b052ed58482d8d7d84f3a0f49f0ef26eca
Anas Ahmad <82609189+ansahmd123@users.noreply.github.com>
2023-10-14 18:55:34 +0000

    trying

--- Hello.py
+++ Hello.py
@@ -1,51 +1,29 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from pytube import YouTube
+import os
 
-LOGGER = get_logger(__name__)
+download_path = os.path.join(os.path.expanduser('~'), 'downloads')
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+def main():
+	path = st.text_input('Enter URL of any youtube video')
+	option = st.selectbox(
+     'Select type of download',
+     ('audio', 'highest_resolution', 'lowest_resolution'))
+	
+	matches = ['audio', 'highest_resolution', 'lowest_resolution']
+	if st.button("download"): 
+		video_object =  YouTube(path)
+		st.write("Title of Video: " + str(video_object.title))
+		st.write("Number of Views: " + str(video_object.views))
+		if option=='audio':
+			video_object.streams.get_audio_only().download() 		#base64.b64encode("if file is too large").decode()	
+		elif option=='highest_resolution':
+			video_object.streams.get_highest_resolution()
+			video_object.download(download_path)
+		elif option=='lowest_resolution':
+			video_object.streams.get_lowest_resolution()
+	if st.button("view"): 
+		st.video(path) 
+if __name__ == '__main__':
+	main()
+	
